237f2f634d34396b4d091f5f5a5c9a5c17fbfaa4
Kunal Pawar <97142774+Kp4103@users.noreply.github.com>
2024-08-06 20:45:56 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,69 +9,64 @@ const app = express();
 const port = process.env.PORT || 4000;
 
 (async () => {
-  try {
-    const version = 'v0.10.26';
-    state.logger = pino({ mixin() { return { version }; } }, pino.destination('logs.txt'));
-    let autoSaver = setInterval(() => storage.save(), 5 * 60 * 1000);
-    ['SIGINT', 'uncaughtException', 'SIGTERM'].forEach((eventName) => process.on(eventName, async (err) => {
-      clearInterval(autoSaver);
-      state.logger.error(err);
-      state.logger.info('Exiting!');
-      if (['SIGINT', 'SIGTERM'].includes(err)) {
-        await storage.save();
-      }
-      process.exit();
-    }));
-
-    state.logger.info('Starting');
-
-    await utils.updater.run(version);
-    state.logger.info('Update checked.');
-
-    const conversion = await utils.sqliteToJson.convert();
-    if (!conversion) {
-      state.logger.error('Conversion failed!');
-      process.exit(1);
-    }
-    state.logger.info('Conversion completed.');
-
-    state.settings = await storage.parseSettings();
-    state.logger.info('Loaded settings.');
-
+  const version = 'v0.10.26';
+  state.logger = pino({ mixin() { return { version }; } }, pino.destination('logs.txt'));
+  let autoSaver = setInterval(() => storage.save(), 5 * 60 * 1000);
+  ['SIGINT', 'uncaughtException', 'SIGTERM'].forEach((eventName) => process.on(eventName, async (err) => {
     clearInterval(autoSaver);
-    autoSaver = setInterval(() => storage.save(), state.settings.autoSaveInterval * 1000);
-    state.logger.info('Changed auto save interval.');
+    state.logger.error(err);
+    state.logger.info('Exiting!');
+    if (['SIGINT', 'SIGTERM'].includes(err)) {
+      await storage.save();
+    }
+    process.exit();
+  }));
 
-    state.contacts = await storage.parseContacts();
-    state.logger.info('Loaded contacts.');
+  state.logger.info('Starting');
 
-    state.chats = await storage.parseChats();
-    state.logger.info('Loaded chats.');
+  await utils.updater.run(version);
+  state.logger.info('Update checked.');
 
-    state.lastMessages = await storage.parseLastMessages();
-    state.logger.info('Loaded last messages.');
-
-    state.dcClient = await discordHandler.start();
-    state.logger.info('Discord client started.');
-
-    await utils.discord.repairChannels();
-    await discordHandler.setControlChannel();
-    state.logger.info('Repaired channels.');
-
-    await whatsappHandler.start();
-    state.logger.info('WhatsApp client started.');
-
-    app.get('/', (req, res) => {
-      res.send('Bot is running');
-    });
-
-    app.listen(port, () => {
-      console.log(`App listening on port ${port}`);
-      state.logger.info(`App listening on port ${port}`);
-    });
-
-  } catch (error) {
-    console.error('An error occurred:', error);
+  const conversion = await utils.sqliteToJson.convert();
+  if (!conversion) {
+    state.logger.error('Conversion failed!');
     process.exit(1);
   }
+  state.logger.info('Conversion completed.');
+
+  state.settings = await storage.parseSettings();
+  state.logger.info('Loaded settings.');
+
+  clearInterval(autoSaver);
+  autoSaver = setInterval(() => storage.save(), state.settings.autoSaveInterval * 1000);
+  state.logger.info('Changed auto save interval.');
+
+  state.contacts = await storage.parseContacts();
+  state.logger.info('Loaded contacts.');
+
+  state.chats = await storage.parseChats();
+  state.logger.info('Loaded chats.');
+
+  state.lastMessages = await storage.parseLastMessages();
+  state.logger.info('Loaded last messages.');
+
+  state.dcClient = await discordHandler.start();
+  state.logger.info('Discord client started.');
+
+  await utils.discord.repairChannels();
+  await discordHandler.setControlChannel();
+  state.logger.info('Repaired channels.');
+
+  await whatsappHandler.start();
+  state.logger.info('WhatsApp client started.');
+
+  app.get('/', (req, res) => {
+    res.send('Bot is running');
+  });
+
+  app.listen(port, () => {
+    console.log(`App listening on port ${port}`);
+    state.logger.info(`App listening on port ${port}`);
+  });
+
 })();
